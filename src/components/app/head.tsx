9a8ea9b864dbb8399f5dfc726842efb1c08daff5
Pranav Kanade <pranav@indorse.io>
2019-12-30 01:39:40 +0530

    Added the global head

---
+++ src/components/app/head.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const AppHead = () => {
+  return (
+    <Head>
+      <title>Show Up!</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=Heebo|Noto+Sans+SC|Nunito+Sans&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+  );
+};
+
+export default AppHead;
